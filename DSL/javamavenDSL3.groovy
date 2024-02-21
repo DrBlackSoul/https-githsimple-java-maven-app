4622aaf80fc40b503ea54974f3899865bad2f49c
DrBlackSoul <147674323+DrBlackSoul@users.noreply.github.com>
2024-02-20 23:07:06 -0300

    Update javamavenDSL3.groovy

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/DrBlackSoul/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('DrBlackSoul')
+            node / gitConfigEmail('matiasnfiordaliso@gmail.com')
         }
     }
     triggers {
